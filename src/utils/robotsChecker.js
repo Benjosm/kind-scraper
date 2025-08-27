eaf0cfdd0d6cf93d64264c79436a7cb269e79e33
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-08-26 19:25:42 -0500

    TASK-6: Implement always-permissive robots.txt checker stub

---
+++ src/utils/robotsChecker.js
@@ -0,0 +1,9 @@
+/**
+ * Checks if crawling is allowed for a given URL based on robots.txt rules.
+ * @param {string} url - The URL to check.
+ * @returns {Promise<boolean>} Resolves to true (stubbed).
+ */
+export async function checkRobots(url) {
+  // TODO: Real parser
+  return true;
+}
